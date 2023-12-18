8bf7ddf322664d27401fbf97c44f45196d170424
reza <rezaizadij2000@gmail.com>
2023-12-18 13:13:20 +0100

    change: simplifie the problem

--- lib/dataset.h
+++ lib/dataset.h
@@ -16,32 +16,18 @@ struct Point
     Point(int pointId, int pointValue);
 };
 
-struct Cluster
-{
-    int id;
-    int mean;
-    std::vector<Point> data;
-
-    Cluster(int id);
-    Cluster(int id, std::vector<Point> data);
-    Cluster() : id(1), data(1, Point(0, 0)) {}
-    // void calculateMean();
-};
-
 struct Dataset
 {
     int numberOfPoints;
     int numberOfClusters;
     int maxIteration;
-    std::vector<Point> pointList;
-    std::vector<Cluster> clusterList;
+    std::vector<int> pointList;
     std::string filename;
 
     Dataset(int numberOfPoints, int numberOfClusters, int maxIteration, std::string filename);
 
     void generateData();
     void readData(std::string filename);
-    void showDataPoints();
-    void showTheClusterData();
+
     void createClusters(int rank, MPI_Comm comm);
 };
